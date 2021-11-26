2d9a40f0b36d00078c231a447be0237814883d2c
E-Waste <moistjesus@admins-MacBook-Pro.local>
2021-11-26 00:06:23 -0800

    Completed Buble sort

--- sorting.c
+++ sorting.c
@@ -24,6 +24,40 @@ void printArr(int arr[], int ARRSIZE){
 }
 
 
+void Run_Sorting_Algorithms(int ARRSIZE){
+    // Creating a massive array of random integers between 0 and 300
+    //int ARRSIZE = 60000;
+    int intArr[ARRSIZE];
+
+    for(int x = 0; x <= ARRSIZE; x++){
+        intArr[x] = rand() % 300;
+        // printf("%d\t%d\n", x, intArr[x]);
+    }
+
+    // Insetion sort stuff
+    int insertionArr[ARRSIZE];          // Copy Array
+    for(int x = 0; x <= ARRSIZE; x++){
+        insertionArr[x] = intArr[x];
+        // printf("%d\t%d\n", x, intArr[x]);
+    }
+    insertionsort(insertionArr, ARRSIZE);
+
+    // Selection sort stuff
+    int selectionArr[ARRSIZE];          // Copy Array
+    for(int x = 0; x <= ARRSIZE; x++){
+        selectionArr[x] = intArr[x];
+    }
+    selectionSort(selectionArr, ARRSIZE);
+
+    // Bubble sort stuff
+    int bubbleArr[ARRSIZE];
+    for(int x = 0; x <= ARRSIZE; x++){
+        bubbleArr[x] = intArr[x];
+    }
+    bubbleSort(bubbleArr, ARRSIZE);
+}
+
+
 /****
  * Insertion Sort
 ****/
@@ -85,6 +119,29 @@ void selectionSort(int selectionArr[], int ARRSIZE){
 /****
  * Bubble Sort
 ****/
+void bubbleSort(int bubbleArr[], int ARRSIZE){
+    clock_t start = clock();
+
+    // For ARRSIZE - 1 number of passes
+    for(int i = 0; i < ARRSIZE; i++){
+        // All the items less than i are already sorted so we iterate past them
+        for (int j = 0; j <= ARRSIZE - 1 - i; j++){
+            if(bubbleArr[j] > bubbleArr[j + 1]){
+                // Swap the two elements if true
+                int temp = bubbleArr[j + 1];
+                bubbleArr[j + 1] = bubbleArr[j];
+                bubbleArr[j] = temp;
+            }
+        }
+    }
+
+    clock_t end = clock();
+
+    //printArr(bubbleArr, ARRSIZE);
+
+    double runtime = Calculate_Runtime(end, start);
+    printf("\nBubble Sort completed:\t\t%f seconds\n", runtime);
+}
 
 
 /****
--- sorting.h
+++ sorting.h
@@ -17,8 +17,11 @@
 float Calculate_Runtime(clock_t end, clock_t start);
 void printArr(int arr[], int ARRSIZE);
 
+void Run_Sorting_Algorithms(int ARRSIZE);
+
 void insertionsort(int insertionArr[], int ARRSIZE);
 void selectionSort(int selectionArr[], int ARRSIZE);
+void bubbleSort(int bubbleArr[], int arrSize);
 
 
 
